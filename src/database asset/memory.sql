7f0dbcbebf54e90c7dee9fd1975ae8299c8976bd
XavJad597 <92932067+XavJad597@users.noreply.github.com>
2023-06-17 16:49:29 -0300

    Create memory.sql
    database code

---
+++ src/database asset/memory.sql
@@ -0,0 +1,41 @@
+create  database memoryG;
+use memoryG;
+
+create table player (
+player_id int auto_increment primary key,
+player_name     varchar(50),
+birth_date      date,
+username      varchar(50) unique,
+password      varchar(50)
+);
+
+create table game (
+game_id    int primary key,     /* primary key */       
+player_id    int , /*foreign key*/
+start_time     time(6),
+end_time   time (6),
+score       int 
+);
+
+
+create table activity (
+activity_id     int auto_increment primary key ,/*primary key*/
+player_id       int ,/*foreign key*/
+game_id         int,  /*foreign key*/
+round_play       int
+);
+
+
+create table game_levels (
+game_level_id       int auto_increment primary key,
+player_id           int,
+game_id             int ,
+easy_level               int,
+medium_level           int,
+hard_level               int
+);
+
+
+           
+-- foreign keys ---
+
