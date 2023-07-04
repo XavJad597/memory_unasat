78bae4bb86d48126c19948c24a29ac6039746d22
XavJad597 <92932067+XavJad597@users.noreply.github.com>
2023-07-04 17:47:30 -0300

    Update memory.sql

--- src/database asset/memory.sql
+++ src/database asset/memory.sql
@@ -11,24 +11,20 @@ p_password      varchar(50)
 create table game (
 game_id    int auto_increment primary key,     /* primary key */       
 player_id    int , /*foreign key*/
-score       int 
+score         int,
+rounds_played    int 
 );
 
 
 create table activity (
 activity_id     int auto_increment primary key ,/*primary key*/
-player_id       int ,/*foreign key*/
-game_id         int,  /*foreign key*/
-round_play       int
+player_id       int ,  /*foreign key*/
+game_id         int,   /*foreign key*/
+rounds_played      int
 );
 
 
-create table game_levels (
-game_level_id       int auto_increment primary key,
-player_id           int,
-game_id             int ,
-game_played          int
-);
+
 
 
            
@@ -44,13 +40,6 @@ add foreign key (player_id) references player(player_id);
 alter table activity
 add foreign key (game_id) references game(game_id);
 
-
-alter table game_levels
-add foreign key (game_id) references game(game_id);
-
-
-alter table game_levels
-add foreign key (player_id) references player(player_id);
 
 -- end foreign key ---
 
